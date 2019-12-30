41e7f3257512bf06bded4bdb6eb9072092da79b0
Paresh Dudhat <dudhatparesh@gmail.com>
2019-12-30 19:29:24 +0530

    added delay interface

--- common/src/iosMain/kotlin/com/example/demo/dispatchers/AppDispatchersImpl.kt
+++ common/src/iosMain/kotlin/com/example/demo/dispatchers/AppDispatchersImpl.kt
@@ -1,18 +1,15 @@
 package com.example.demo.dispatchers
 
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.*
 import platform.Foundation.NSRunLoop
 import platform.Foundation.performBlock
-import platform.darwin.dispatch_get_main_queue
-import platform.darwin.dispatch_queue_t
+import platform.darwin.*
 import kotlin.coroutines.CoroutineContext
 import kotlin.native.concurrent.SharedImmutable
 
 @InternalCoroutinesApi
-actual class AppDispatchersImpl : AppDispatchers {
+actual class AppDispatchersImpl : AppDispatchers, CoroutineDispatcher(), Delay {
     @SharedImmutable
     override val main: CoroutineDispatcher =
         NsQueueDispatcher(dispatch_get_main_queue())
@@ -31,5 +28,45 @@ actual class AppDispatchersImpl : AppDispatchers {
         }
     }
 
+
+    @InternalCoroutinesApi
+    override fun scheduleResumeAfterDelay(timeMillis: Long, continuation: CancellableContinuation<Unit>) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, timeMillis * 1_000_000), dispatch_get_main_queue()) {
+            try {
+                with(continuation) {
+                    resumeUndispatched(Unit)
+                }
+            } catch (err: Throwable) {
+                throw err
+            }
+        }
+    }
+
+    @InternalCoroutinesApi
+    override fun invokeOnTimeout(timeMillis: Long, block: Runnable): DisposableHandle {
+        val handle = object : DisposableHandle {
+            var disposed = false
+                private set
+
+            override fun dispose() {
+                disposed = true
+            }
+        }
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, timeMillis * 1_000_000), dispatch_get_main_queue()) {
+            try {
+                if (!handle.disposed) {
+                    block.run()
+                }
+            } catch (err: Throwable) {
+                throw err
+            }
+        }
+
+        return handle
+    }
+
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        
+    }
 
 }
